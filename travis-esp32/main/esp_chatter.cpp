4230b5ee5f48a8fb9ba928be3a8009d0661cc7c5
hunarislam <hunar.islam01@gmail.com>
2023-07-04 03:32:55 +0530

    odom and cmd_vel kinda funct

--- travis-esp32/main/esp_chatter.cpp
+++ travis-esp32/main/esp_chatter.cpp
@@ -3,22 +3,33 @@
 #include "/home/foodl/esp/esp-idf/components/rosserial_esp32/include/geometry_msgs/TransformStamped.h"
 #include "esp_chatter.h"
 #include <string>
-#include <tf/transform_broadcaster.h>
+// #include <tf/transform_broadcaster.h>
+#include <math.h>
 
 using namespace std;
 
 ros::NodeHandle nh;
 
-nav_msgs::Odometry odom;
-geometry_msgs::TransformStamped odom_tf;
-tf::TransformBroadcaster tf_broadcaster;
-
-ros::Publisher odom_pub("odom", &odom);
-ros::Subscriber<geometry_msgs::Twist> cmd_vel_sub("cmd_vel", cmdVelCallback);
+nav_msgs::Odometry odomMsg;
+// tf::TransformBroadcaster tf_broadcaster;
+ros::Publisher odom_pub("odom", &odomMsg);
 
 float last_time = 0.0;
 float linear_x = 0.0;
 float angular_z = 0.0;
+
+// Odometry variables
+float x = 0.0;
+float y = 0.0;
+float theta = 0.0;
+
+void cmdVelCallback(const geometry_msgs::Twist& cmd_vel)
+{
+  linear_x = cmd_vel.linear.x;
+  angular_z = cmd_vel.angular.z;
+}
+
+ros::Subscriber<geometry_msgs::Twist> cmd_vel_sub("cmd_vel", cmdVelCallback);
 
 void rosserialSetup()
 {
@@ -28,13 +39,7 @@ void rosserialSetup()
   nh.subscribe(cmd_vel_sub);
 }
 
-void cmdVelCallback(const geometry_msgs::Twist& cmd_vel)
-{
-  linear_x = cmd_vel.linear.x;
-  angular_z = cmd_vel.angular.z;
-}
-
-void getCmdVel(float& vel_x, float& vel_z)
+void getCmdVel(float* vel_x, float* vel_z)
 {
   *vel_x = linear_x;
   *vel_z = angular_z;
@@ -47,45 +52,62 @@ void publishOdometry(float vel_x, float vel_z)
 
   // Calculate the time elapsed since the last call
   static ros::Time last_time = current_time;
-  float dt = (current_time - last_time).toSec();
+  float dt = current_time.toSec() - last_time.toSec();
 
-  // Set the header
-  odom.header.stamp = nh.now();
-  odom.header.frame_id = "odom";
-  odom.child_frame_id = "base_footprint";
-
-  // Calculate the distance traveled
-  float distance = vel_x * dt;
-
-  // Calculate the change in orientation
+  // Calculate odometry values
+  float delta_x = vel_x * cos(theta) * dt;
+  float delta_y = vel_x * sin(theta) * dt;
   float delta_theta = vel_z * dt;
 
-  // Update the pose
-  odom.pose.pose.position.x += distance * cos(delta_theta);
-  odom.pose.pose.position.y += distance * sin(delta_theta);
-  odom.pose.pose.orientation.z = sin(delta_theta / 2);
-  odom.pose.pose.orientation.w = cos(delta_theta / 2);
+  x += delta_x;
+  y += delta_y;
+  theta += delta_theta;
 
-  // Update the twist
-  odom.twist.twist.linear.x = vel_x;
-  odom.twist.twist.angular.z = vel_z;
+  // Create and populate the Odometry message
+  odomMsg.header.stamp = nh.now();
+  odomMsg.header.frame_id = "odom";
+  odomMsg.child_frame_id = "base_link";
 
-  // Update the timestamp
-  odom.header.stamp = current_time;
+  // Position
+  odomMsg.pose.pose.position.x = x;
+  odomMsg.pose.pose.position.y = y;
+  odomMsg.pose.pose.position.z = 0.0;
+
+  // Orientation
+  odomMsg.pose.pose.orientation.x = 0.0;
+  odomMsg.pose.pose.orientation.y = 0.0;
+  odomMsg.pose.pose.orientation.z = sin(theta / 2.0);
+  odomMsg.pose.pose.orientation.w = cos(theta / 2.0);
+
+  // Linear velocity
+  odomMsg.twist.twist.linear.x = vel_x;
+  odomMsg.twist.twist.linear.y = 0.0;
+  odomMsg.twist.twist.linear.z = 0.0;
+
+  // Angular velocity
+  odomMsg.twist.twist.angular.x = 0.0;
+  odomMsg.twist.twist.angular.y = 0.0;
+  odomMsg.twist.twist.angular.z = vel_z;
 
   // Publish the Odometry message
-  odom_pub.publish(&odom);
+  odom_pub.publish(&odomMsg);
 
   // Calculate and publish the transform
-  odom_tf.header.stamp = odom.header.stamp;
-  odom_tf.header.frame_id = odom.header.frame_id;
-  odom_tf.child_frame_id = odom.child_frame_id;
-  odom_tf.transform.translation.x = odom.pose.pose.position.x;
-  odom_tf.transform.translation.y = odom.pose.pose.position.y;
-  odom_tf.transform.translation.z = odom.pose.pose.position.z;
-  odom_tf.transform.rotation = odom.pose.pose.orientation;
+  // geometry_msgs::TransformStamped odom_tf;
 
-  tf_broadcaster.sendTransform(odom_tf);
+  // odom_tf.header.stamp = nh.now();;
+  // odom_tf.header.frame_id = "odom";
+  // odom_tf.child_frame_id = "base_link";
+  // odom_tf.transform.translation.x = x;
+  // odom_tf.transform.translation.y = y;
+  // odom_tf.transform.translation.z = 0.0;
+  // odom_tf.transform.rotation.x = 0.0;
+  // odom_tf.transform.rotation.y = 0.0;
+  // odom_tf.transform.rotation.z = sin(theta / 2.0);
+  // odom_tf.transform.rotation.w = cos(theta / 2.0);
+
+
+  // tf_broadcaster.sendTransform(odom_tf);
 
   // Update the last call time
   last_time = current_time;
